66e4c4ee55e1e4dd095a592f449501b9f9b0a270
Abhinav Khanna <AbhinavKhanna@outlook.com>
2020-03-27 06:53:23 -0500

    printing file content

--- src/main.ts
+++ src/main.ts
@@ -24,14 +24,23 @@ async function main(): Promise<void> {
 
     const octokit = new github.GitHub(gitHubToken)
 
-    console.log(
-      await octokit.repos.getContents({
-        owner: gitHubRepoOwner,
-        repo: gitHubRepoName,
-        path,
-        ref: gitHubRepoBranch
-      })
-    )
+    const {status, data: response} = await octokit.repos.getContents({
+      owner: gitHubRepoOwner,
+      repo: gitHubRepoName,
+      path,
+      ref: gitHubRepoBranch
+    })
+
+    if (status >= 200 || status < 300) {
+      if (!Array.isArray(response)) {
+        const fileContent: string = response.content ? response.content : ''
+        console.log(Buffer.from(fileContent, 'base64'))
+      } else {
+        core.setFailed('Path might be pointing to a directory. Please provide a file path.')
+      }
+    } else {
+      core.setFailed('Error in fetching the shared action file. Please check the input values.')
+    }
 
     // octokit.repos.getContents({path: path})
   } catch (error) {
